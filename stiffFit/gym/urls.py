d31a0df7eaa756319a491539ab5a11d1c2fc557e
Rafsan2001 <63017429+Rafsan2001@users.noreply.github.com>
2021-11-25 02:09:20 +0600

    register and login pagee final

--- stiffFit/gym/urls.py
+++ stiffFit/gym/urls.py
@@ -6,6 +6,12 @@ from . import views
 
 urlpatterns = [
 
-	path('register/', views.registerPage, name="register"),
-    path('login/', views.loginPage, name="login"),
+	path('' ,views.home  , name="home"),
+    path('register/' , views.register_attempt , name="register_attempt"),
+    path('login/' , views.login_attempt , name="login_attempt"),
+    path('token/' , views.token_send , name="token_send"),
+    path('success/' , views.success , name='success'),
+    path('verify/<auth_token>' , views.verify , name="verify"),
+    path('error/' , views.error_page , name="error")
+
 ]
--- stiffFit/stiffFit/settings.py
+++ stiffFit/stiffFit/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gym'
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,21 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#SMTP Configuration
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='eilearn321@gmail.com'
+EMAIL_HOST_PASSWORD='poro1234'
+#eilearn321@gmail.com
+#password : poro1234
