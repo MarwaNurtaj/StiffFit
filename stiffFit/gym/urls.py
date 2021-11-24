9b7da58d7b18cd1c0e15060a58deec47edbf71dd
Rafsan2001 <63017429+Rafsan2001@users.noreply.github.com>
2021-11-25 03:18:09 +0600

    marged

--- stiffFit/gym/urls.py
+++ stiffFit/gym/urls.py
@@ -6,10 +6,21 @@ from . import views
 
 urlpatterns = [
 
+<<<<<<< HEAD
 
-    path('', views.home, name="home"),
+    
     path('trainer/', views.trainer, name="trainer"),
     path('trainee/', views.trainee, name="trainee"),
-	path('register/', views.registerPage, name="register"),
-    path('login/', views.loginPage, name="login"),
+	
+   
+=======
+	path('' ,views.home  , name="home"),
+    path('register/' , views.register_attempt , name="register_attempt"),
+    path('login/' , views.login_attempt , name="login_attempt"),
+    path('token/' , views.token_send , name="token_send"),
+    path('success/' , views.success , name='success'),
+    path('verify/<auth_token>' , views.verify , name="verify"),
+    path('error/' , views.error_page , name="error")
+
+>>>>>>> origin/rafsan
 ]
