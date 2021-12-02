5091ce07830f1bed42d5e8f2b70daa9dab342e5b
MarwaNurtaj <94914325+MarwaNurtaj@users.noreply.github.com>
2021-12-02 15:39:03 +0600

    created models shown in admin

--- stiffFit/gym/admin.py
+++ stiffFit/gym/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display=('alt_test')
+admin.site.register(models.Banners)
+
+admin.site.register(models.Profile)
+
+
