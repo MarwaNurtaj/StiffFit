538bbefa5dbadd0828f4708f192de54c68a97c03
MarwaNurtaj <94914325+MarwaNurtaj@users.noreply.github.com>
2021-12-23 16:01:34 +0600

    marged from dev

--- stiffFit/gym/admin.py
+++ stiffFit/gym/admin.py
@@ -3,9 +3,29 @@ from . import models
 # Register your models here.
 
 class BannerAdmin(admin.ModelAdmin):
-    list_display=('alt_test')
+    list_display=('alt_test','image_tag')
 admin.site.register(models.Banners)
 
-admin.site.register(models.Profile)
+admin.site.register(models.Profile) 
+admin.site.register(models.Trainer) 
+admin.site.register(models.Trainee)
+admin.site.register(models.Package) 
+admin.site.register(models.Progress) 
+
+class NotifyAdmin(admin.ModelAdmin):
+    list_display=('notify_detail', 'read_by_user')
+admin.site.register(models.Notify,NotifyAdmin)    
 
 
+
+class PageAdmin(admin.ModelAdmin):
+    list_display=('alt_test',)
+admin.site.register(models.Page)
+
+class FaqAdmin(admin.ModelAdmin):
+    list_display=('quest',)
+admin.site.register(models.Faq,FaqAdmin)
+
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display=('full_name','email','detail',)
+admin.site.register(models.Enquiry,EnquiryAdmin)
--- stiffFit/gym/forms.py
+++ stiffFit/gym/forms.py
@@ -3,7 +3,14 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import *
 class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username','email','password1','password2']
+
+
+class EnquiryForm(forms.ModelForm):
+	class Meta:
+		model=Enquiry
+		fields=('full_name','email','detail')
