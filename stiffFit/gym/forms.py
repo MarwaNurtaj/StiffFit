9f26fbc1d33ccc7c742759ae8e625e298c65c550
Rafsan Bari Shafin <shafinrafsan46@gmail.com>
2022-01-03 02:21:32 +0600

    trainer login added

--- stiffFit/gym/forms.py
+++ stiffFit/gym/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 from django import forms
 from .models import *
 from . import models
@@ -21,3 +21,8 @@ class ProfileForm(UserChangeForm):
 	class Meta:
 		model=User
 		fields=('first_name','last_name','email','username')
+
+class TrainerLoginForm(forms.ModelForm):
+	class Meta:
+		model=models.Trainer
+		fields=('username','pwd')
