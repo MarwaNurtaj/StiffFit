e6e68a5c00383be65969273ed5d3d5cec26a681b
MarwaNurtaj <94914325+MarwaNurtaj@users.noreply.github.com>
2022-01-03 18:22:54 +0600

    Subscription added

--- stiffFit/gym/models.py
+++ stiffFit/gym/models.py
@@ -49,17 +49,7 @@ class Trainer(models.Model):
     def __str__(self):
         return self.trainer
     
-class Trainee(models.Model):
-	user=models.ForeignKey(User, on_delete=models.CASCADE,null=True)
-	mobile=models.CharField(max_length=20,null=True)
-	address=models.TextField(null=True)
-	img=models.ImageField(upload_to="trainee/",null=True)
 
-	def __str__(self):
-		return str(self.user)
-
-	def image_tag(self):
-			return mark_safe('<img src="%s" width="80" />' % (self.img.url))
 
 
     
@@ -82,7 +72,6 @@ class Progress(models.Model):
                 ('Progressing', 'Progressing' ),
                 ('Completed', 'Completed'),
 )
-    #trainee = models.ForeignKey(Trainee, null=True, on_delete=models.SET_NULL)
     trainer = models.ForeignKey(Trainer, null=True, on_delete=models.SET_NULL)
     package = models.ForeignKey(Package, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
@@ -111,10 +100,6 @@ class Faq(models.Model):
     def __str__(self):
         return self.quest
 
-class Enquiry(models.Model):
-    full_name=models.CharField(max_length=150)
-    email=models.CharField(max_length=150)
-    detail=models.TextField()
 
     def __str__(self):
         return self.full_name
